0c260db9c65337f9cc99d9b026ac0f206d19f08a
Thiago Alves <thiagoa@amazon.com>
2019-10-25 15:10:35 -0500

    Split the plugin in multiple files
    ## Changes Description
    
    The plugin was trying to support bash and zsh on the same codebase.
    Although this should be ok, there are certain behaviors from bash that
    have no relevance to zsh and vice-versa.
    
    This commit split the plugin on a generic library only containning the
    common code between platforms.
    
    Than we split even further to have a file per platform we desire to
    support; Ideally, other shells can have their support implemented by
    including the common code library and create a small setup scrip
    tailored for their case.

--- virtualenv-autodetect.plugin.zsh
+++ virtualenv-autodetect.plugin.zsh
@@ -1,1 +1,14 @@
-virtualenv-autodetect.sh
+# virtualenv-autodetect.plugin.zsh
+#
+# Installation:
+# Add this line to your .zshenv
+#
+# source /path/to/virtualenv-autodetect.plugin.zsh
+
+SCRIPT_DIR=${0:a:h}
+
+. $SCRIPT_DIR/virtualenv-autodetect-lib.sh
+
+# Activate on directory change.
+# Zsh.
+chpwd_functions+=(_virtualenv_auto_activate)
